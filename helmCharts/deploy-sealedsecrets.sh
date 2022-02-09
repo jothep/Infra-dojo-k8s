052fb9df69836dccd8fc56d38f793d4fca0e15db
xzhu-tw <xzhu@thoughtworks.com>
2022-02-09 11:46:29 +0800

    change ss ns

--- helmCharts/deploy-sealedsecrets.sh
+++ helmCharts/deploy-sealedsecrets.sh
@@ -3,7 +3,7 @@
 helm upgrade sealedsecrets ./sealed-secrets \
   --install \
   --create-namespace \
-  --namespace sealedsecrets \
+  --namespace kube-system \
   --values sealed-secrets/values.yaml \
   --values sealed-secrets/extra-values.yaml \
   --set podLabels.label1=mylabel
